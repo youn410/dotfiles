a0f928dfc6e62a5f18caa9ac6890b54232f14dc5
youn410 <youn410@gmail.com>
2026-08-22 20:55:09 +0900

    fix: pin Intel Darwin to Nixpkgs 26.05

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Nix configuration for dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-parts = {
